619e760dfddccda8c9102eb528a773998d1d6ca5
AndriyGoncharuk <160170957+AndriyGoncharuk@users.noreply.github.com>
2024-06-23 15:40:59 +0300

    asdf

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,99 +1,84 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 const images = [
   {
-     preview:
+     large:
         'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
-     original:
+     small:
         'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
      description: 'Hokkaido Flower',
   },
   {
-     preview:
+     large:
         'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-     original:
+     small:
         'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
      description: 'Container Haulage Freight',
   },
   {
-     preview:
+     large:
         'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-     original:
+     small:
         'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
      description: 'Aerial Beach View',
   },
   {
-     preview:
+     large:
         'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-     original:
+     small:
         'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
      description: 'Flower Blooms',
   },
   {
-     preview:
+     large:
         'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-     original:
+     small:
         'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
      description: 'Alpine Mountains',
   },
   {
-     preview:
+     large:
         'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-     original:
+     small:
         'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
      description: 'Mountain Lake Sailing',
   },
   {
-     preview:
+     large:
         'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-     original:
+     small:
         'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
      description: 'Alpine Spring Meadows',
   },
   {
-     preview:
+     large:
         'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-     original:
+     small:
         'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
     description: 'Nature Landscape',
   },
   {
-    preview:
+    large:
         'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-    original:
+    small:
         'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
     description: 'Lighthouse Coast Sea',
   },
 ];
 
-const gallery = document.querySelector('.gallery');
-const galleryMarkup = images.map(({ preview, original, description }) => {
-    return `
-        <li class="gallery-item">
-          <a class="gallery-link" href="${original}">
-            <img
-              class="gallery-image"
-              src="${preview}"
-              data-source="${original}"
-              alt="${description}"
-            />
-          </a>
-        </li>
-      `;
-}).join('');
+const galleryContainer = document.querySelector('.gallery');
+const galleryItems = images.map(({ large, small, description }) => `
+    <li class="gallery-item">
+        <a class="gallery-link" href="${large}">
+            <img class="gallery-image" src="${small}" alt="${description}" />
+        </a>
+    </li>
+`).join('');
+galleryContainer.innerHTML = galleryItems;
 
-gallery.insertAdjacentHTML('beforeend', galleryMarkup);
-
-gallery.addEventListener('click', event => {
-      event.preventDefault();
-      if (event.target.nodeName !== 'IMG') return;
-
-      const source = event.target.dataset.source;
-
-      const instance = basicLightbox.create(`
-        <img src="${source}" width="1112" height="640">
-      `);
-      instance.show();
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom'
 });
-// const lightbox = new SimpleLightbox('.gallery a', { sourceAttr });
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
-    
